ac26fa7c3ac86ca718e6d135450e8de982ea0737
GhostX1407 <jaivinvachhani@gmail.com>
2026-08-22 17:52:56 +0530

    feat: add render.yaml and support persistent db path

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,7 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const path = require('path');
 
-const adapter = new FileSync(path.join(__dirname, '../../db.json'));
+const adapter = new FileSync(process.env.DB_PATH || path.join(__dirname, '../../db.json'));
 const db = low(adapter);
 
 // Set defaults if db.json is empty/new
